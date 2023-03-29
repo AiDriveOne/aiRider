fe82ce3a68cea4df025dfb23b0a3679ea3816f10
AiDrive_Application <96203659+AiDriveOne@users.noreply.github.com>
2023-03-30 01:44:20 +0200

    driver_management
    void assignDeliveries(List<Driver> drivers, List<Route> routes) {
      for (int i = 0; i < drivers.length; i++) {
        Driver driver = drivers[i];
        Route route = routes[i];
    
        // Update driver schedule
        driver.schedule.add(route.deliveryTime);
    
        // Assign deliveries to driver
        for (Order order in route.orders) {
          order.driver = driver;
        }
      }
    }

---
+++ driver_management.dart
@@ -0,0 +1,14 @@
+void assignDeliveries(List<Driver> drivers, List<Route> routes) {
+  for (int i = 0; i < drivers.length; i++) {
+    Driver driver = drivers[i];
+    Route route = routes[i];
+
+    // Update driver schedule
+    driver.schedule.add(route.deliveryTime);
+
+    // Assign deliveries to driver
+    for (Order order in route.orders) {
+      order.driver = driver;
+    }
+  }
+}
